e2b238c6b6d1452cf8f43b3d8481a1665f6bcaa4
stephanielingwood <stephanielingwood@gmail.com>
2017-12-15 17:41:43 -0800

    Add GET and POST routes; tests in progress

--- todos/handler.js
+++ todos/handler.js
@@ -7,3 +7,10 @@ const todos = new Todos(dynamoDb, `Todos-${process.env.STAGE}`);
 module.exports.list = (event, context, callback) => {
   todos.list(callback);
 };
+
+module.exports.post = (event, context, callback) => {
+  const body = JSON.parse(event.body);
+  todos.post(body, callback);
+};
+
+// TODO: select ORM, implement PUT, GET, and DELETE
--- todos/todos.js
+++ todos/todos.js
@@ -1,3 +1,5 @@
+const uuidv4 = require('uuid/v4');
+
 class Todos {
   constructor(db, tableName) {
     this.db = db;
@@ -21,6 +23,38 @@ class Todos {
         const response = {
           statusCode: 200,
           body: JSON.stringify(todos.Items)
+        };
+
+        callback(null, response);
+      }
+    );
+  }
+
+  post(body, callback) {
+    const params = {
+      TableName: this.tableName,
+      Item: {
+        id: uuidv4(), // TODO: Will dynamoDb assign ids?
+        title: body.title,
+        completed: body.completed,
+        updatedAt: Date.now()
+      },
+      ConditionExpression: 'attribute_not_exists(id)',
+      ReturnConsumedCapacity: 'NONE'
+    };
+
+    this.db.put(
+      params,
+      (err, todo) => {
+        if (err) {
+          console.error(err);
+          callback(`Failed to create todo, with error: ${err}`);
+          return;
+        }
+
+        const response = {
+          statusCode: 200,
+          body: JSON.stringify(todo)
         };
 
         callback(null, response);
