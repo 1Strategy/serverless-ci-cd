65ed1624d64e8c421de2948f36ba5ac5dacf338f
stephanielingwood <stephanielingwood@gmail.com>
2017-12-22 17:31:41 -0800

    Add npm install to promotion.sh

--- promotion.sh
+++ promotion.sh
@@ -2,6 +2,11 @@
 
 ls -la
 
+echo "Installing serverless"
+echo "====================="
 npm install -g serverless
+npm install # we shouldn't need this, as serverless-dynamodb-local is a dev dependency, but it's throwing an error without it
 
+echo "Deploying app"
+echo "============="
 serverless deploy --stage $env --package ./artifacts -v
