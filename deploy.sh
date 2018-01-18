b2e9a5de7728402f176ad9cf4286f474d8c0fcc8
stephanielingwood <stephanielingwood@gmail.com>
2018-01-18 12:18:14 -0800

    Install required serverless plugins

--- deploy.sh
+++ deploy.sh
@@ -3,9 +3,8 @@
 echo "Installing serverless"
 echo "====================="
 npm install -g serverless
-npm install serverless-dynamodb-local serverless-offline # we shouldn't need this, as serverless-dynamodb-local is a dev dependency, but it's throwing an error without it
+npm install serverless-dynamodb-local serverless-offline serverless-stack-output # we have to install the three plugins specified in our serverless.yml to avoid errors, even though we won't be using them
 
 echo "Deploying app"
 echo "============="
-
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
