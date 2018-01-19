443bf4250c51883937070240f9e7454f33db653d
stephanielingwood <stephanielingwood@gmail.com>
2018-01-18 16:58:20 -0800

    Add stage name to log

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ echo "====================="
 npm install -g serverless
 npm install serverless-dynamodb-local serverless-offline serverless-stack-output # we have to install the three plugins specified in our serverless.yml to avoid errors, even though we won't be using them
 
-echo "Deploying app"
-echo "============="
+echo "Deploying app to $env"
+echo "====================="
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
